022345f7afa5e48c3cac243111534bc6ddfdd651
WiDuMu <97642890+WiDuMu@users.noreply.github.com>
2023-04-26 22:42:38 -0500

    basic functionallity and dark mode

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 "use strict"
-const name = document.getElementById('lookup');
+/** Fetch the list of TLDs */
+const TLDsRequest = fetch('https://data.iana.org/TLD/tlds-alpha-by-domain.txt');
+const nameInput = document.getElementById('lookup');
+const hacksList = document.getElementById('hacks-list');
+let brandName = nameInput.value;
+
+let TLDs = (await (await TLDsRequest).text()).split('\n').slice(1, -1);
+console.log(TLDs);
+nameInput.addEventListener('input', () => {
+    brandName = nameInput.value; console.log(brandName);
+    let hacks = [];
+    for (const TLD of TLDs) {
+        const lowerBrand = brandName.toLowerCase().trim();
+        const lowerTLD = TLD.toLowerCase();
+        if (lowerBrand.endsWith(lowerTLD)) {
+            const li = document.createElement('li');
+            li.textContent = `${brandName.slice(0, brandName.length - TLD.length)}.${lowerTLD}`;
+            hacks.push(li);
+        }
+    }
+    hacksList.replaceChildren(...hacks);
+});
